becc1a373052615580e08b2579ca394c94708718
lloyd4565 <88751177+lloyd4565@users.noreply.github.com>
2021-11-11 17:31:27 +0200

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -25,9 +25,9 @@ export default class Command extends BaseCommand {
         const length = videos.length < 10 ? videos.length : 10
         let text = `🤡 *Results for ${term}*\n`
         for (let i = 0; i < length; i++) {
-            text += `*#${i + 1}*\n📗 *Title:* ${videos[i].title}\n📕 *Channel:* ${
+            text += `*#${i + 1}*\n❣️ *Title:* ${videos[i].title}\n💝 *Channel:* ${
                 videos[i].author.name
-            }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
+            }\n 🖤 *Duration:* ${videos[i].duration}\n💛 *URL:* ${videos[i].url}\n\n`
         }
         M.reply('💋 Getting the list...')
         this.client
