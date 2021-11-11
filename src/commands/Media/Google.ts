a43884a9d4dee28c32e95aac2d03a4bae8bf8199
lloyd4565 <88751177+lloyd4565@users.noreply.github.com>
2021-11-11 12:08:47 +0200

    Update Google.ts

--- src/commands/Media/Google.ts
+++ src/commands/Media/Google.ts
@@ -17,25 +17,25 @@ export default class Command extends BaseCommand {
     }
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         if (!this.client.config.gkey) return void M.reply('No google API key set')
-        if (!joined) return void M.reply('🔎 Provide a search term')
+        if (!joined) return void M.reply('🤡 Provide a search term')
         const term = joined.trim()
         await axios
             .get(
                 `https://www.googleapis.com/customsearch/v1?q=${term}&key=${this.client.config.gkey}&cx=baf9bdb0c631236e5`
             )
             .then((res) => {
-                if (res.status !== 200) return void M.reply(`🔍 Error: ${res.status}`)
+                if (res.status !== 200) return void M.reply(`🤪 Error: ${res.status}`)
                 let result = ``
                 let index = 1
                 for (const item of res.data?.items) {
-                    result += `*👾${index}.Title* : ${item.title}\n*🔗Link* : ${item.link}\n*📖Snippet* : ${item.snippet}\n\n`
+                    result += `*💋${index}.Title* : ${item.title}\n*🔗Link* : ${item.link}\n*📖Snippet* : ${item.snippet}\n\n`
                     index++
                 }
-                // return void M.reply(`🔍Command Used : ${Command.count} times\n Result for *${term}*\n\n\n ${result}`)
-                return void M.reply(`🔍 Result for *${term}*\n\n\n ${result}`)
+                // return void M.reply(`💀Command Used : ${Command.count} times\n Result for *${term}*\n\n\n ${result}`)
+                return void M.reply(`🤡 Result for *${term}*\n\n\n ${result}`)
             })
             .catch((err) => {
-                M.reply(`🔍 Error: ${err}`)
+                M.reply(`🤪 Error: ${err}`)
             })
     }
 }
